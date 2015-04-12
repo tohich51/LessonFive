d9e0ab58a202152169224ad0863592c124a86220
tohich51 <tohich51@gmail.com>
2015-04-12 09:38:18 +0300

    Initial Commit

---
+++ LessonFive/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LessonFive
+//
+//  Created by oscar on 12/04/15.
+//  Copyright (c) 2015 oscar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
